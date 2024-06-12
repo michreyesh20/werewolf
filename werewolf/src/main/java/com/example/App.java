7e7cbfac0a6122a4f9ba0c69a911c8087533ea3d
whiteCatc <davvf1221@gmail.com>
2024-06-12 10:40:19 -0600

    interfaz Funcionando

--- werewolf/src/main/java/com/example/App.java
+++ werewolf/src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import interfaz.InicioPrincipal;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Werewolf!");
+        InicioPrincipal Intertface = new InicioPrincipal();
+                Intertface.setVisible(true);
     }
 }
